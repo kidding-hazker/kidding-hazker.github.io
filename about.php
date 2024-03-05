68009defbc520f39b2b0e8a6c2dcb92c971e5ab1
kidding <kidding@hazker.kidding-hazker.org>
2024-03-06 02:29:54 +0300

    3rd

--- about.php
+++ about.php
@@ -10,15 +10,10 @@
             <a href="https://kidding-hazker.github.io">kidding-hazker</a>
             <div>
                 <a href="blog.php" id="blog">blog</a>
+                <a href="projects.php" id="projects">projects</a>
                 <a href="https://github.com/kidding-hazker" id="github">github</a>
                 <a href="https://youtube.com/@kidding-hazker" id="youtube">youtube</a>
-                <a href="about.php" id="about">about</a>
             </div>
         </header>
-        <main>
-            <form action="">
-                <input type="text" placeholder="search" name="search_query">
-            </form>
-        </main>
     </body>
 </html>
--- blog.php
+++ blog.php
@@ -9,7 +9,6 @@
         <header>
             <a href="https://kidding-hazker.github.io">kidding-hazker</a>
             <div>
-                <a href="blog.php" id="blog">blog</a>
                 <a href="projects.php" id="projects">projects</a>
                 <a href="https://github.com/kidding-hazker" id="github">github</a>
                 <a href="https://youtube.com/@kidding-hazker" id="youtube">youtube</a>
